af1d5360ca471ccb9c3601f9fd49f1fafad40677
vctoraguilar <vicaguilarpi@upt.pe>
2024-03-30 18:43:46 -0500

    inicial-commit

---
+++ app/src/main/java/com/rasgo/compa_app/feature/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.rasgo.compa_app.feature.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rasgo.compa_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/rasgo/compa_app/feature/homepage/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rasgo.compa_app.feature.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rasgo.compa_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
